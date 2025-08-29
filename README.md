bd7d9263814ffca10e3346dda64594af792dee55
minhdanghorus <dangnguyentranminhk2017@gmail.com>
2025-08-29 16:54:00 +0700

    fix: write data in multiple sharding

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ npm init -y
 # Pre-requisites
 ```bash
 docker build -t pgshard .
-docker run --name pgshard1 -e POSTGRES_PASSWORD=postgres -d -p 5434:5434 pgshard
-docker run --name pgshard2 -e POSTGRES_PASSWORD=postgres -d -p 5435:5435 pgshard
-docker run --name pgshard3 -e POSTGRES_PASSWORD=postgres -d -p 5436:5436 pgshard
+docker run --name pgshard1 -e POSTGRES_PASSWORD=postgres -d -p 5434:5432 pgshard
+docker run --name pgshard2 -e POSTGRES_PASSWORD=postgres -d -p 5435:5432 pgshard
+docker run --name pgshard3 -e POSTGRES_PASSWORD=postgres -d -p 5436:5432 pgshard
 ```
 
 # Install dependencies
--- index.js
+++ index.js
@@ -18,35 +18,38 @@ const clients = {
         password: 'postgres',
         database: 'postgres'
     }),
-    // '5435': new Client({
-    //     host: 'localhost',
-    //     port: 5435,
-    //     user: 'postgres',
-    //     password: 'postgres',
-    //     database: 'postgres'
-    // }),
-    // '5436': new Client({
-    //     host: 'localhost',
-    //     port: 5436,
-    //     user: 'postgres',
-    //     password: 'postgres',
-    //     database: 'postgres'
-    // }),
+    '5435': new Client({
+        host: 'localhost',
+        port: 5435,
+        user: 'postgres',
+        password: 'postgres',
+        database: 'postgres'
+    }),
+    '5436': new Client({
+        host: 'localhost',
+        port: 5436,
+        user: 'postgres',
+        password: 'postgres',
+        database: 'postgres'
+    }),
 }
 
 connect();
 async function connect() {
     await clients['5434'].connect();
-    // await clients['5435'].connect();
-    // await clients['5436'].connect();
+    await clients['5435'].connect();
+    await clients['5436'].connect();
 }
 
-app.get('/', (req, res) => {
+app.post('/', async (req, res) => {
     const url = req.query.url;
     // ex: http://localhost:8081/?url=https://wikipedia.com/sharding
     const hash = crypto.createHash("sha256").update(url).digest("hex");
     const urlId = hash.substring(0, 5);
     const server = hr.get(urlId);
+
+    await clients[server].query(`INSERT INTO URL_TABLE (URL, URL_ID) VALUES ($1, $2)`, [url, urlId]);
+
     res.send({
         "urlId": urlId,
         "url": url,
